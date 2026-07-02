22f68e9e843d610dc2bdf99705d63c95379e7e17
aryankatiyar2323 <ryankatiyar2323@gmail.com>
2026-07-02 21:56:54 +0530

    Complete authentication improvements

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -6,67 +6,73 @@ import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-
   try {
-
     const { name, email, password } = req.body;
 
     // Check if email already exists
     const existingUser = await db.query(
       "SELECT * FROM users WHERE email = $1",
-      [email]
+      [email],
     );
 
     if (existingUser.rows.length > 0) {
-
       return res.status(400).json({
-        message: "Email already exists"
+        message: "User already exists",
       });
-
     }
 
     // Hash password
-    const hashedPassword = await bcrypt.hash(
-      password,
-      10
-    );
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // Insert user
-    await db.query(
+    const insertUser = await db.query(
       `
       INSERT INTO users
       (name, email, password)
       VALUES
       ($1, $2, $3)
+      RETURNING id
       `,
-      [name, email, hashedPassword]
+      [name, email, hashedPassword],
+    );
+
+    const token = jwt.sign(
+      {
+        id: insertUser.rows[0].id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      },
     );
 
     res.status(201).json({
-      message: "User registered successfully"
+      message: "User registered successfully",
+      token
     });
-
   } catch (error) {
-        console.log(error.response.data);
-    }
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
 });
 
 router.post("/login", async (req, res) => {
   try {
-
     // Get email and password from frontend
     const { email, password } = req.body;
 
     // Find user by email
-    const userExist = await db.query(
-      "SELECT * FROM users WHERE email = $1",
-      [email]
-    );
+    const userExist = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
 
     // Email not found
     if (userExist.rowCount === 0) {
       return res.status(400).json({
-        message: "Email not found"
+        message: "Invalid email or password",
       });
     }
 
@@ -74,44 +80,37 @@ router.post("/login", async (req, res) => {
     const hashedPassword = userExist.rows[0].password;
 
     // Compare entered password with hash
-    const isMatch = await bcrypt.compare(
-      password,
-      hashedPassword
-    );
+    const isMatch = await bcrypt.compare(password, hashedPassword);
 
     // Password incorrect
     if (!isMatch) {
       return res.status(400).json({
-        message: "Invalid Password"
+        message: "Invalid email or password",
       });
     }
     // Creating token
     const token = jwt.sign(
       {
-        id: userExist.rows[0].id
+        id: userExist.rows[0].id,
       },
-        process.env.JWT_SECRET,
+      process.env.JWT_SECRET,
       {
-        expiresIn: "7d"
-      }
+        expiresIn: "7d",
+      },
     );
 
     // Login success
     res.status(200).json({
       message: "Login Successful",
-      token
+      token,
     });
-
   } catch (error) {
-
     console.log(error);
 
     res.status(500).json({
-      message: "Server Error"
+      message: "Server Error",
     });
-
   }
 });
-
 
 export default router;
